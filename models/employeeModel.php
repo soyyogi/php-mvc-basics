3b4493564b5898f17039fd8c0448281c28471898
soyyogi <raoyogesh78@yahoo.in>
2021-02-23 10:59:37 +0100

    added mysqli connection

--- models/employeeModel.php
+++ models/employeeModel.php
@@ -1,11 +1,32 @@
 <?php
 
-function get(){
-    $data = 'Add all employees data from database';
+function get()
+{
+    $con = mysqli_connect('localhost', 'yogi', 'archdb', 'employees', 3306, '/run/mysqld/mysqld.sock');
+
+    // Check connection
+    if (mysqli_connect_errno()) {
+        echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
+        exit();
+    }
+
+    $sql = 'SELECT first_name, last_name, gender, hire_date FROM employees ORDER BY first_name ASC';
+    $result = mysqli_query($con, $sql);
+
+    // Fetch all
+    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
+
+    // Free result set
+    mysqli_free_result($result);
+
+    mysqli_close($con);
+  
     return $data;
 }
 
-function getById($id){
+function getById($id)
+{
     $data = 'Add employee data from database';
     return $data;
 }
